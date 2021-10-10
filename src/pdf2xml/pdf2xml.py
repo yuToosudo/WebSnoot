b9d043f480360f753f2e571552846c30c23058e4
Bryan Speelman <bryan.speelman@steampunk.com>
2021-10-10 19:58:06 -0400

    Add tokenizer

--- src/pdf2xml/pdf2xml.py
+++ src/pdf2xml/pdf2xml.py
@@ -13,3 +13,22 @@ filename = sys.argv[0]
 #Extract the text from PDF
 text = extract_text(filename)
 print(text)
+# Create an instance of tokenizer using NLTK ResexpTokenizer
+#
+tokenizer = RegexpTokenizer('\w+')
+print(tokenizer)
+#
+# Tokenize the text read from PDF
+#
+tokens = tokenizer.tokenize(text)
+print(tokens)
+#
+# Find Frequency Distribution
+#
+freqdist = FreqDist(tokens)
+print(freqdist)
+#
+# Find words whose length is greater than 5 and frequency greater than 20
+#
+long_frequent_words = [words for words in tokens if len(words) > 5 and freqdist[words] > 20]
+print(long_frequent_words)
